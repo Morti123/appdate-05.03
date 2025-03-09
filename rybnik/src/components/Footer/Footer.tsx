22eb0420e5183861f1d46629db56df54e56a673e
Morti123 <bogdanbronovitsky@gmail.com>
2025-03-09 16:00:42 +0300

    09.03

--- rybnik/src/components/Footer/Footer.tsx
+++ rybnik/src/components/Footer/Footer.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../../components/styles/global.css'
 import './Footer.css'
+import TelegramIcon from '@mui/icons-material/Telegram';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 const Footer: React.FC = () => {
   return (
@@ -8,12 +12,12 @@ const Footer: React.FC = () => {
       <div className="footer_container">
         <div className="info">
           <div className="icons">
-            <a className="bi-instagram" id="insta" href="https://www.instagram.com/bronovitskiialeksandr?igsh=bDhyOXcyYzZ1ZG5k"></a>
-            <a className="bi-telegram" id="tg" href=" https://t.me/+375296857651"></a>
-            <a className="bi-whatsapp" id="vb" href="viber://chat?number=375296857651"></a>
+            <a href="https://www.instagram.com/bronovitskiialeksandr?igsh=bDhyOXcyYzZ1ZG5k"><InstagramIcon className="icon" id="insta"></InstagramIcon></a>
+            <a href=" https://t.me/+375296857651"><TelegramIcon className="icon" id="tg"></TelegramIcon></a>
+            <a href="viber://chat?number=375296857651"><WhatsAppIcon  className="icon" id="vb" ></WhatsAppIcon></a>
           </div>
           <div className="tel">
-            <a className="bi-telephone-fill" id="tel">+375 29 685-76-51</a>
+            <div className="cont"><PhoneIcon  className="i" id="tel"></PhoneIcon><p>+375 29 685-76-51</p></div>
           </div>
         </div>
         <div className="service">
